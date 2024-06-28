aa767fc9fb0d2101d2f6e23faf751d5222c80eb6
maxwellmoura <maxwellcmoura@gmail.com>
2024-06-28 15:54:36 -0300

    Subindo Novo Protifolio

---
+++ src/pages/Projetos/Projetos.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import './Projetos.css'
+
+const Projetos = () => {
+    return (
+        <section className="projetos section" id="projetos">
+            <div className="container">
+                <div className="row">
+                    <div className="section-title">
+                        <h2>Meus projetos</h2>
+                    </div>
+                </div>
+
+                <div className="container-projetos">
+                    <div className="projeto">
+                        <a href="https://maxwellmoura.github.io/cursodev/Sindicato/">
+                            <img src="./src/assets/sindicato.PNG" alt="Site" />
+                            <h1>Projeto 1</h1>
+                            <div className="informacoes-projeto">
+                                <h2>Site</h2>
+                                <p>Projeto feito usando HTML e CSS</p>
+                                <p>🔗 Ver no GitHub Pages</p>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div className="projeto">
+                        <a href="https://maxwellmoura.github.io/cursodev/LandingPage/">
+                            <img src="./src/assets/landpage.PNG" alt="LandingPage" />
+                            <h1>Projeto 2</h1>
+                            <div className="informacoes-projeto">
+                                <h2>LandingPage</h2>
+                                <p>Projeto feito usando HTML e CSS</p>
+                                <p>🔗 Ver no GitHub Pages</p>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div className="projeto">
+                        <a href="https://maxwellmoura.github.io/cursodev/rock/">
+                            <img src="./src/assets/rock.PNG" alt="Seach de Bandas" />
+                            <h1>Projeto 3</h1>
+                            <div className="informacoes-projeto">
+                                <h2>Projeto de Seach</h2>
+                                <p>Projeto feito usando HTML e CSS</p>
+                                <p>🔗 Ver no GitHub Pages</p>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div className="projeto">
+                        <a href="https://maxwellmoura.github.io/cursodev/agencia/">
+                            <img src="./src/assets/agencia.PNG" alt="Agencia de Viagem" />
+                            <h1>Projeto 4</h1>
+                            <div className="informacoes-projeto">
+                                <h2>Agencia de Viagem</h2>
+                                <p>Projeto feito usando HTML e CSS</p>
+                                <p>🔗 Ver no GitHub Pages</p>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div className="projeto">
+                        <a href="https://mini-blog-seven-xi.vercel.app/">
+                            <img src="./src/assets/miniblog.PNG" alt="MiniBlog" />
+                            <h1>Projeto 5</h1>
+                            <div className="informacoes-projeto">
+                                <h2>MiniBlog</h2>
+                                <p>Projeto feito usando HTML, CSS, <br />
+                                    JavaScript, React, FireBase</p>
+                                <p>🔗 Ver no GitHub Pages</p>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div className="projeto">
+                        <a href="https://mini-blog-seven-xi.vercel.app/">
+                            <img src="./src/assets/lanchonete.PNG" alt="Lanchonete" />
+                            <h1>Projeto 6</h1>
+                            <div className="informacoes-projeto">
+                                <h2>Lanchonete</h2>
+                                <p>Projeto feito usando HTML, CSS</p>
+                                <p>🔗 Ver no GitHub Pages</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Projetos
